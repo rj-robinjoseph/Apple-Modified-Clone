c5b75ed7b22c67867c3a82d14e17c9a618184d44
rj-robinjoseph <robinjoseph452@gmail.com>
2022-04-05 18:00:33 +0530

    Slider-Finished

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-const slides = document.querySelectorAll('.slide');
-const next = document.querySelector('#next');
-const prev = document.querySelector('#prev');
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
 const auto = true; // Auto scroll
 const intervalTime = 5000;
 let slideInterval;
 
 const nextSlide = () => {
   // Get current class
-  const current = document.querySelector('.current');
+  const current = document.querySelector(".current");
   // Remove current class
-  current.classList.remove('current');
+  current.classList.remove("current");
   // Check for next slide
   if (current.nextElementSibling) {
     // Add current to next sibling
-    current.nextElementSibling.classList.add('current');
+    current.nextElementSibling.classList.add("current");
   } else {
     // Add current to start
-    slides[0].classList.add('current');
+    slides[0].classList.add("current");
   }
-  setTimeout(() => current.classList.remove('current'));
+  setTimeout(() => current.classList.remove("current"));
 };
 
 const prevSlide = () => {
   // Get current class
-  const current = document.querySelector('.current');
+  const current = document.querySelector(".current");
   // Remove current class
-  current.classList.remove('current');
+  current.classList.remove("current");
   // Check for prev slide
   if (current.previousElementSibling) {
     // Add current to prev sibling
-    current.previousElementSibling.classList.add('current');
+    current.previousElementSibling.classList.add("current");
   } else {
     // Add current to last
-    slides[slides.length - 1].classList.add('current');
+    slides[slides.length - 1].classList.add("current");
   }
-  setTimeout(() => current.classList.remove('current'));
+  setTimeout(() => current.classList.remove("current"));
 };
 
 // Button events
-next.addEventListener('click', e => {
+next.addEventListener("click", (e) => {
   nextSlide();
   if (auto) {
     clearInterval(slideInterval);
@@ -46,7 +46,7 @@ next.addEventListener('click', e => {
   }
 });
 
-prev.addEventListener('click', e => {
+prev.addEventListener("click", (e) => {
   prevSlide();
   if (auto) {
     clearInterval(slideInterval);
